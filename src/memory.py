ce07cb3df88d6649e01d15f112799f369630fe43
Lars De Volder <36573562+beverneus@users.noreply.github.com>
2025-03-18 11:33:31 +0100

    use python list for memory instead of numpy array

--- src/memory.py
+++ src/memory.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class Memory:
     def __init__(self, size):
-        self.data = np.zeros(size)
+        self.data = list(np.zeros(size))
 
     def write(self, address, byte):
         self.data[address] = byte
